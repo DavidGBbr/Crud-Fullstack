b29ca3fba0050eb06e105cb0a972a15b5597fbe9
davidGBbr <davidgomesbrigido@gmail.com>
2023-10-23 13:03:20 -0300

    backend - deletando usuário

--- api/src/controllers/api.Controller.ts
+++ api/src/controllers/api.Controller.ts
@@ -40,3 +40,14 @@ export const updateUser = async (req: Request, res: Response) => {
     res.json({ error: "Usuário não encontrado." });
   }
 };
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  let user = await User.findByPk(id);
+  if (user) {
+    await user.destroy();
+    res.status(200).json({ status: "Usuário deletado com sucesso!" });
+  } else {
+    res.json({ error: "Usuário não encontrado." });
+  }
+};
--- api/src/routes/api.ts
+++ api/src/routes/api.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.get("/", ApiController.getUsers);
 router.post("/add", ApiController.addUser);
 router.put("/update/:id", ApiController.updateUser);
+router.delete("/delete/:id", ApiController.deleteUser);
 
 export default router;
